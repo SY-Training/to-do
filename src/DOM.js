d1a1e67f49ee12bc1e9790310d134cd34c192f68
sy <sy-unimportant@outlook.com>
2021-12-21 08:46:21 +0000

    Third time completely redoing code from scratch. Focused on initial layout first, then will code. Redsigned page layout.

--- src/DOM.js
+++ src/DOM.js
@@ -1,87 +1,25 @@
-import projectFactory from "./index";
 
-const content = document.querySelector('#content');
-const projects = [];
 
-function dom () {
+const headerBox = document.querySelector('.header-box');
+const nav = document.querySelector('.nav');
+const taskBox = document.querySelector('.task-box');
 
-    const projectList = document.createElement('div');
-    projectList.setAttribute('class', 'projectList');
-    projectList.innerHTML = `<p> TEST!!! </p>`;
-    content.appendChild(projectList);
 
-    const myButton = document.createElement('button');
-    myButton.setAttribute('id', 'create');
-    myButton.innerText = "Create";
-    content.appendChild(myButton);
+function Landing () {
+    const projectTitle = document.createTextNode(`Projects`);
+    nav.appendChild(projectTitle);
 
-    myButton.addEventListener('click', addProject)
+    const landingHeadText = document.createTextNode('Welcome');
+    headerBox.appendChild(landingHeadText);
+
+    const landingTaskText = document.createTextNode(`Hmm, you don't have any projects`);
+    taskBox.appendChild(landingTaskText);
 
 }
 
-function addProject(){
-    let proName = prompt("Project name: ");
-    let proDesc = prompt("Description: ");
-    let proDate = prompt("When is it due: ");
-
-    let newPro = projectFactory(proName, proDesc, proDate);
-    projects.push(newPro);
-
-    console.log(projects);
-
-    listProjects();
-
-}
-
-function listProjects() {
-
-    const listTop = document.querySelector('.projectList');
-    removeProjects();
 
 
-    projects.forEach(obj =>{
-
-        let pro = document.createElement('div');
-        pro.setAttribute('class', 'project');
-
-        // Create left side of project, name and desc.
-        let proLeft = document.createElement('div');
-        proLeft.setAttribute('class', 'proLeft');
-        proLeft.innerHTML = `<p> ${obj.name} </p>`;
-        let text = document.createTextNode(`${obj.description}`);
-        proLeft.appendChild(text);
-        pro.appendChild(proLeft);
-        
-        // Create right side of project, due date and create task.
-        let proRight = document.createElement('div');
-        proRight.setAttribute('class', 'proRight');
-        let dueDate = document.createElement('div')
-        dueDate.setAttribute('class', 'dueDate');
-        dueDate.innerHTML = `${obj.due}`;
-        proRight.appendChild(dueDate);
-        pro.appendChild(proRight);
-
-        let newTask = document.createElement('button');
-        newTask.setAttribute('class', 'newTask');
-        newTask.textContent = `+`;
-        proRight.appendChild(newTask);
-
-        // Add all to project list.
-        listTop.appendChild(pro);      
-
-    });
-}
-
-function removeProjects() {
-    const list = document.querySelector('.projectList');
-    while (list.firstChild){
-        list.removeChild(list.lastChild);
-    }
-    
-}
-
-export { 
-    dom,
-    addProject,
+export {
+    Landing
 
 }
--- src/index.js
+++ src/index.js
@@ -1,21 +1,5 @@
-import { dom } from './DOM.js';
 import './style.css';
+import {Landing} from './DOM.js'
 
-dom();
-
-// Change prompt to html form. 
-// add due date. 
-// add priority colour.
-// Add tasks which add to the object
-
-// Maybe add regex in title to limit char ammount.
-  // -- same for desc.
-
-
-function projectFactory (name, description, due) {
-    
-    return {name, description, due};
-}
-
-export default projectFactory;
+Landing();
 
