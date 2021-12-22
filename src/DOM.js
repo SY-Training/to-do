a9e0f4316032344625aee54534c4549ee262ed79
sy <sy-unimportant@outlook.com>
2021-12-22 12:26:29 +0000

    Created new project button and functions. Projects new display in left pane.

--- src/DOM.js
+++ src/DOM.js
@@ -1,13 +1,12 @@
-
+import { myProjects, NewProjectPrompt } from "./index.js";
 
 const headerBox = document.querySelector('.header-box');
 const nav = document.querySelector('.nav');
 const taskBox = document.querySelector('.task-box');
+const newProject = document.querySelector('.newProject');
 
 
 function Landing () {
-    const projectTitle = document.createTextNode(`Projects`);
-    nav.appendChild(projectTitle);
 
     const landingHeadText = document.createTextNode('Welcome');
     headerBox.appendChild(landingHeadText);
@@ -15,11 +14,29 @@ function Landing () {
     const landingTaskText = document.createTextNode(`Hmm, you don't have any projects`);
     taskBox.appendChild(landingTaskText);
 
+    newProject.addEventListener('click', NewProjectPrompt);
+
+}
+
+function UpdateProjectView() {
+    // get myProjects
+    let projectList = myProjects();
+
+    projectList.forEach(obj => {
+        let pro = document.createElement('div');
+        pro.setAttribute('class', 'projects');
+        let proName = document.createTextNode(`${obj.title}`);
+        pro.appendChild(proName);
+        nav.appendChild(pro);
+    })
+    
+    // ForEach project
+    // createElement and add to nav.
 }
 
 
-
 export {
-    Landing
+    Landing,
+    UpdateProjectView,
 
 }
--- src/index.js
+++ src/index.js
@@ -1,5 +1,38 @@
 import './style.css';
-import {Landing} from './DOM.js'
+import {Landing, UpdateProjectView} from './DOM.js'
 
 Landing();
 
+
+// when selecting object, clicking a new task button will add a task to that project.
+// So selecting a project will load it in task view.
+// THEN, clicking new task will have the object selected.
+// projects[i].task = taskDescription. 
+
+const projects = [];
+
+function myProjects() {
+    return projects;
+}
+
+function NewProjectPrompt() {
+    let title = prompt("Project Title: ");
+
+    const newProject = CreateProject(title);
+    projects.push(newProject);
+    console.log(projects);
+
+    UpdateProjectView();
+
+}
+
+function CreateProject(title) {
+    return {
+        title
+    }
+}
+
+export {
+    myProjects,
+    NewProjectPrompt,
+}
