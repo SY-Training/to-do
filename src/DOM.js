d74bc47f53e387e95da9ce0fbb543265c9b51e10
sy <sy-unimportant@outlook.com>
2021-12-29 13:13:27 +0000

    Made progress on local storage, but needs finished

--- src/DOM.js
+++ src/DOM.js
@@ -1,98 +1,106 @@
-import { myProjects, NewProjectPrompt, createTask, DeleteProject, } from "./index.js";
+import {
+  myProjects,
+  NewProjectPrompt,
+  createTask,
+  DeleteProject,
+  DeleteTask,
+} from "./index.js";
 
-const headerBox = document.querySelector('.header-box');
-const nav = document.querySelector('.projectList');
-const taskBox = document.querySelector('.task-box');
-const newProject = document.querySelector('.newProject');
+const headerBox = document.querySelector(".header-box");
+const nav = document.querySelector(".projectList");
+const taskBox = document.querySelector(".task-box");
+const newProject = document.querySelector(".newProject");
 
+function Landing() {
+  const landingHeadText = document.createTextNode("Welcome");
+  headerBox.appendChild(landingHeadText);
 
-function Landing () {
+  const landingTaskText = document.createTextNode(
+    `Create a project, then add tasks here!`
+  );
+  taskBox.appendChild(landingTaskText);
 
-    const landingHeadText = document.createTextNode('Welcome');
-    headerBox.appendChild(landingHeadText);
-
-    const landingTaskText = document.createTextNode(`Create a project, then add tasks here!`);
-    taskBox.appendChild(landingTaskText);
-
-    newProject.addEventListener('click', NewProjectPrompt);
-
+  newProject.addEventListener("click", NewProjectPrompt);
+  UpdateProjectView();
 }
 
 function UpdateProjectView() {
+  let projectList = myProjects();
 
-    let projectList = myProjects();
+  while (nav.firstChild) {
+    nav.removeChild(nav.lastChild);
+  }
 
-    while (nav.firstChild){
-        nav.removeChild(nav.lastChild);
-    }
+  projectList.forEach((obj) => {
+    let pro = document.createElement("button");
+    pro.setAttribute("id", `${obj.title}`);
+    pro.setAttribute("class", "projects");
+    pro.addEventListener("click", function () {
+      LoadRightPane(`${obj.title}`);
+    });
+    pro.textContent = `${obj.title}`;
+    // Add delete button.
+    let del = document.createElement("button");
+    del.setAttribute("class", "proDel");
+    del.textContent = "Delete Project";
+    del.addEventListener("click", function () {
+      DeleteProject(`${obj.title}`);
+    });
+    pro.appendChild(del);
 
-    projectList.forEach(obj => {
-        let pro = document.createElement('button');
-        pro.setAttribute('id', `${obj.title}`);
-        pro.setAttribute('class', 'projects');
-        pro.addEventListener("click", function(){LoadRightPane(`${obj.title}`)});
-        pro.textContent = `${obj.title}`;
-        // Add delete button.
-        let del = document.createElement('button');
-        del.setAttribute('class', 'proDel');
-        del.textContent = "Delete Project";
-        del.addEventListener("click", function() {DeleteProject(`${obj.title}`)})
-        pro.appendChild(del);
-
-        nav.appendChild(pro);
-    })
-    
+    nav.appendChild(pro);
+  });
 }
 
 function LoadRightPane(name) {
+  if (taskBox.firstChild) {
+    taskBox.removeChild(taskBox.firstChild);
+  }
 
-    if (taskBox.firstChild){
-        taskBox.removeChild(taskBox.firstChild);
-    }
-
-
-    let newTask = document.createElement('button');
-    newTask.setAttribute('class', 'newTask');
-    newTask.textContent = `New Task`;
-    newTask.addEventListener("click", function() {createTask(name);});
-    taskBox.appendChild(newTask);
+  let newTask = document.createElement("button");
+  newTask.setAttribute("class", "newTask");
+  newTask.textContent = `New Task`;
+  newTask.addEventListener("click", function () {
+    createTask(name);
+  });
+  taskBox.appendChild(newTask);
 }
 
 function UpdateTaskView(proName) {
+  let projectList = myProjects();
 
-    let projectList = myProjects();
+  while (taskBox.firstChild) {
+    taskBox.removeChild(taskBox.lastChild);
+  }
 
-    while (taskBox.firstChild){
-        taskBox.removeChild(taskBox.lastChild);
+  projectList.forEach((obj) => {
+    if (obj.title === proName) {
+      Object.values(obj.tasks).forEach((task) => {
+        let taskList = document.createElement("div");
+        taskList.setAttribute("class", "taskList");
+        let taskText = document.createTextNode(task);
+        taskList.appendChild(taskText);
+        // Add delete button to task.
+        let del = document.createElement("button");
+        del.setAttribute("class", "taskDel");
+        del.textContent = "Delete Task";
+        del.addEventListener("click", function () {
+          DeleteTask(task);
+        });
+        taskList.appendChild(del);
+        // Add checkbox to task.
+        let check = document.createElement("input");
+        check.setAttribute("type", "checkbox");
+        check.addEventListener("change", function () {
+          if (this.checked) {
+            taskList.style.setProperty("text-decoration", "line-through");
+          }
+        });
+        taskList.appendChild(check);
+        taskBox.appendChild(taskList);
+      });
     }
-
-    projectList.forEach(obj => {
-        if (obj.title === proName){
-
-            let vals = Object.values(obj.tasks).forEach(task => {
-                let taskList = document.createElement('div');
-                taskList.setAttribute('class', 'taskList');
-                let taskText = document.createTextNode(task);
-                taskList.appendChild(taskText);
-                // Adding checkbox to task.
-                let check = document.createElement('input');
-                check.setAttribute('type', 'checkbox');
-                check.addEventListener('change', function() {
-                    if (this.checked){
-                        taskList.style.setProperty('text-decoration', 'line-through');
-                    }
-                })
-                taskList.appendChild(check);
-                taskBox.appendChild(taskList);
-            })            
-            
-        }
-    })
+  });
 }
 
-export {
-    Landing,
-    UpdateProjectView,
-    UpdateTaskView,
-
-}
+export { Landing, UpdateProjectView, UpdateTaskView };
--- src/index.js
+++ src/index.js
@@ -1,62 +1,57 @@
-import './style.css';
-import {Landing, UpdateProjectView, UpdateTaskView} from './DOM.js';
+import "./style.css";
+import { Landing, UpdateProjectView, UpdateTaskView } from "./DOM.js";
 
-Landing();
-
-// TO DO 
-// - Implement saving.
-
+// TO DO
+// - Implement saving. - Finish off
 
 // when selecting object, clicking a new task button will add a task to that project.
 // So selecting a project will load it in task view.
 // THEN, clicking new task will have the object selected.
-// projects[i].task = taskDescription. 
+// projects[i].task = taskDescription.
 
 const projects = [];
 
 function myProjects() {
-    return projects;
+  return projects;
 }
 
 function NewProjectPrompt() {
-    let title = prompt("Project Title: ");
+  let title = prompt("Project Title: ");
 
-    const newProject = CreateProject(title);
-    projects.push(newProject);
-    console.log(projects);
+  const newProject = CreateProject(title);
+  projects.push(newProject);
+  console.log(projects);
 
-    UpdateProjectView();
-
+  UpdateProjectView();
+  saveToLocalStorage();
 }
 
 function CreateProject(title) {
-    return {
-        title,
-        tasks : { },
-    }
+  return {
+    title,
+    tasks: {},
+  };
 }
 
 function DeleteProject(name) {
-    const index = projects.findIndex((pro) => {
-        return pro.title === name;
-    })
+  const index = projects.findIndex((pro) => {
+    return pro.title === name;
+  });
 
-    projects.splice(index, 1);
-    console.log(index);
+  projects.splice(index, 1);
+  UpdateProjectView();
 }
 
 function createTask(proName) {
-    
-    let description = prompt("Task: ")
-    // let due date.
-    let t = 0;
+  let description = prompt("Task: ");
+  // let due date.
+  let t = 0;
 
-    projects.forEach(obj =>{
-        if (obj.title == proName){
+  projects.forEach((obj) => {
+    if (obj.title == proName) {
+      let size = Object.keys(obj.tasks).length;
 
-            let size = Object.keys(obj.tasks).length
-
-            /*  -- This is if I don't want to use the nested object for tasks. 
+      /*  -- This is if I don't want to use the nested object for tasks. 
                     Also remove nexted prop from create project. 
                     And change obj.tasks to just obj above.
 
@@ -64,24 +59,33 @@ function createTask(proName) {
                 obj[`task${i}`] = description;
             }
             */
-            
-            for (t = 0; t < size; t++)
-            {
 
-            }
+      for (t = 0; t < size; t++) {}
 
-            obj.tasks[`task${t}`] = description;            
-        }
-    })
-    
-    UpdateTaskView(proName);
+      obj.tasks[`task${t}`] = description;
+    }
+  });
 
+  UpdateTaskView(proName);
 }
 
-
-export {
-    myProjects,
-    NewProjectPrompt,
-    createTask,
-    DeleteProject,
+function DeleteTask(name) {
+  console.log(name);
 }
+
+const saveToLocalStorage = () => {
+  localStorage.setItem("projects", JSON.stringify(projects));
+};
+
+const loadLocalStorage = () => {
+  if (projects.length > 0) {
+    JSON.parse(localStorage.getItem("projects"));
+  } else {
+    console.log("empty");
+  }
+};
+
+loadLocalStorage();
+Landing();
+
+export { myProjects, NewProjectPrompt, createTask, DeleteProject, DeleteTask };
